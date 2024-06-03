d741bd62fb530330f8819b1222f9cc1e70b1fa50
Philip Karanja <kpk.my.mailings@gmail.com>
2024-06-03 15:56:14 -0500

    Update script.js

--- pyramidGenerator/script.js
+++ pyramidGenerator/script.js
@@ -1,14 +1,27 @@
 const character = "5";
 const count = 8;
-const rows = ["Naomi", "Quincy", "CamperChan"];
+const rows = [];
 
-console.log(character);
+//practicing functions
+function padRow() {
+
+}
+
+const call = padRow();
 
 //practicing for loops
 for (let i = 0; i < count; i = i + 1) {
-    console.log(i)
+    rows.push(character);
 
 }
+
+let result = ""
+
+for (const row of rows) {
+    result = result + "\n" + row;
+    
+}
+
 
 //.length knows length of array
 //.push adds element to end of an array. When logged in console it returns the new length of the array
