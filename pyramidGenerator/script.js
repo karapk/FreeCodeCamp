78686f7d876a6fb78a1e9160bf285e226325d551
Philip Karanja <kpk.my.mailings@gmail.com>
2024-06-03 02:08:20 -0500

    Update script.js

---
+++ pyramidGenerator/script.js
@@ -0,0 +1,15 @@
+const character = "5";
+const count = 8;
+const rows = ["Naomi", "Quincy", "CamperChan"];
+
+console.log(character);
+
+//practicing for loops
+for (let i = 0; i < count; i = i + 1) {
+    console.log(i)
+
+}
+
+//.length knows length of array
+//.push adds element to end of an array. When logged in console it returns the new length of the array
+//.pop removes last element from an array and returns that element
