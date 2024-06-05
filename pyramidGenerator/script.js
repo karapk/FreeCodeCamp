ee6c8a3531307cd069da76ac7c9f7b5c2abfc611
Philip Karanja <kpk.my.mailings@gmail.com>
2024-06-05 05:42:49 -0500

    Update script.js

--- pyramidGenerator/script.js
+++ pyramidGenerator/script.js
@@ -8,12 +8,35 @@ function padRow(rowNumber, rowCount) {
 }
 
 //practicing for loops
-for (let i = 0; i < count; i = i + 1) {
-    rows.push(padRow(i +1, count));
+// for (let i = 0; i < count; i = i + 1) {
+//     rows.push(padRow(i +1, count));
 
+// }
+
+//making code cleaner by using addition assignment operator shorthand
+// for (let i = 0; i < count; i += 1) {
+//     rows.push(padRow(i +1, count));
+
+// }
+
+/*using the increment operator ++ for the same code to make it even more shorthand
+Changed i to start at one hence didn't need to do i+1
+since we lost a row while starting at 1 an <= operator has been added to fix this*/
+// for (let i = 1; i <= count; i ++) {
+//     rows.push(padRow(i, count));
+
+// }
+
+let continueLoop = false;
+let done = 0;
+
+while (done !== count) {
+    done++
+    rows.push(padRow(done, count));
+    if (done === count) {
+        continueLoop =false;
+    }
 }
-
-
 
 let result = ""
 
